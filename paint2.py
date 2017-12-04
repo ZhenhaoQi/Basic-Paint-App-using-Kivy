aba1dce9e1e71fb8a845c45f3d84848f2c323473
Beni Madhav Agrawal <benimadhavagrawal@gmail.com>
2017-12-04 15:44:56 +0000

    Add files via upload

--- paint2.py
+++ paint2.py
@@ -16,7 +16,7 @@ xboun=0
 yboun=0
 rect=False
 press=0
-width=5
+wide=5
 def reset():
     global rect, ell, sline
     rect=ell=sline=False
@@ -24,9 +24,17 @@ def retclr():
     return clr
 
 def incanvasxy(self,x,y):
-    if x > 0.15* self.width and y >0.2 *self.height and x < self.width*0.75 and y < self.height*0.8:
+    if x > 0.25* self.width and y >0.2 *self.height and x < self.width*0.75 and y < self.height*0.8:
         return True
-
+def default (self):
+    with self.canvas.after:
+        col =[1,1,1,1]    
+        Color(*col)
+        Line(rectangle=(0.25* self.width-13, 0.2 *self.height-13, self.width*0.5 +26, self.height*0.6+26),width=13)
+    with self.canvas:
+        col =[0,0,0,1]    
+        Color(*col)
+        Line(rectangle=(0.25* self.width-25, 0.2 *self.height-25, self.width*0.5 +50, self.height*0.6+50),width=25)
 
 class Painter(Widget):
     
@@ -37,7 +45,7 @@ class Painter(Widget):
 
     def on_touch_down(self, touch):
         #print "down"
-        global xs,ys,xboun,yboun,press
+        global xs,ys,xboun,yboun,press,wide
         press = 1
         if incanvasxy(self,touch.x,touch.y):   
             
@@ -51,16 +59,17 @@ class Painter(Widget):
                 Color(*self.col)
                 #d = 30
                 #Ellipse(pos=(touch.x - d / 2,touch.y - d / 2), size=(d,d))
-                touch.ud['line'] = Line(points=(touch.x, touch.y),width=5)
+                touch.ud['line'] = Line(points=(touch.x, touch.y),width=wide)
                 #if sline:
                 xs=touch.x
                 ys=touch.y
         else:
             xs=touch.x
             ys=touch.y
+        default(self)    
     def on_touch_move(self, touch):
         #print "move"
-        global xs,ys,xboun,yboun
+        global xs,ys,xboun,yboun,wide
         if incanvasxy(self,touch.x,touch.y) and incanvasxy(self,xs,ys) :
             self.col= retclr()
             if sline:
@@ -72,7 +81,7 @@ class Painter(Widget):
                      #   Color(*[0,0,0,1])
                       #  Line(points=(xp,yp,xs,ys),width=4)
                     Color(*self.col)
-                    Line(points=(touch.x,touch.y,xs,ys),width=4)
+                    Line(points=(touch.x,touch.y,xs,ys),width=wide)
                 xboun=touch.x
                 yboun=touch.y
             elif rect:
@@ -81,17 +90,17 @@ class Painter(Widget):
                 with self.canvas.after:
                     self.canvas.after.clear()
                     Color(*self.col)
-                    Line(rectangle=(xs, ys, touch.x-xs, touch.y-ys),width=4)
+                    Line(rectangle=(xs, ys, touch.x-xs, touch.y-ys),width=wide)
                 xboun=touch.x
                 yboun=touch.y
                     
             else:
                 if incanvasxy(self,xs,ys):
                     touch.ud["line"].points += [touch.x, touch.y]
-
+        default(self)
     def on_touch_up(self, touch):
         #print "up"
-        global xs,ys,press
+        global xs,ys,press,wide
         
         if incanvasxy(self,xs,ys):
             if incanvasxy(self,touch.x,touch.y) :    
@@ -104,7 +113,7 @@ class Painter(Widget):
                         return True
                     with self.canvas:
                         Color(*self.col)
-                        Line(points=(touch.x,touch.y,xs,ys),width=4) 
+                        Line(points=(touch.x,touch.y,xs,ys),width=wide) 
                 if rect:
                     
                     self.col= retclr()
@@ -113,22 +122,23 @@ class Painter(Widget):
                         return True
                     with self.canvas:
                         Color(*self.col)
-                        Line(rectangle=(xs, ys, touch.x-xs, touch.y-ys),width=4)         
+                        Line(rectangle=(xs, ys, touch.x-xs, touch.y-ys),width=wide)         
             else:
                 if press:
                     if sline :
                         with self.canvas:
                                 if xboun:
                                     Color(*self.col)
-                                    Line(points=(xboun,yboun,xs,ys),width=4)
+                                    Line(points=(xboun,yboun,xs,ys),width=wide)
                         self.canvas.after.clear()
                     if rect :
                         with self.canvas:
                                 if xboun:
                                     Color(*self.col)
-                                    Line(rectangle=(xs, ys, xboun-xs, yboun-ys),width=4)
+                                    Line(rectangle=(xs, ys, xboun-xs, yboun-ys),width=wide)
                         self.canvas.after.clear() 
-
+        self.canvas.after.clear()
+        default(self)                    
         press=0                     
 class Cpicker(ColorPicker):
     pass
@@ -138,15 +148,13 @@ class popup(Popup):
         global clr,pre_clr
         pre_clr=clr    
         clr=y 
-class popup2(Popup):
-    def thick (self, w):
-        global width 
-        width =w
+
 
 class MainScreen(Screen):
     
     def open_it1(self):
         popup().open()
+
     def open_it2(self):
         popup2().open()
     
@@ -156,6 +164,10 @@ class MainScreen(Screen):
         pre_clr = clr
         clr= [0,0,0,1]
     
+    def thick(self,*args):
+        global wide
+        wide = args[1]
+        self.lab.text = "Width : " + str(args[1])
     def pencil(self):
         global clr,pre_clr,sline
         clr=pre_clr
